150f227ff648f512d32f53c89abafbeee127f85f
Reshav Ravi <140899268+lawnge10@users.noreply.github.com>
2026-09-12 11:47:14 +0530

    till conversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)//we are using node.js not browser
+//when can we use spaces and semicolon
+//code readabilty should be good
+let name="Reshav"
+let age = 18
+let isLoggedIn=false
+
+//number => 2 to power 33
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//underfined=>value not assigned
+//symbol =>to find uniquness
+
+
+//object 
+console.log(typeof "Reshav");
+//type of null is object
