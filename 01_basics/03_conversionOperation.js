9b15b57d7f6982d4ee305c8f33d654ab78e6b81f
Reshav Ravi <140899268+lawnge10@users.noreply.github.com>
2026-09-12 11:49:11 +0530

    till conversion and operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score="33"
+console.log(typeof(score));
+let valueInNumber=Number(score)//typecasting or conversion into other data types
+
+
+//"33" =>33 after typecasting
+//"33abc"=>NaN
+
+let isLoggedIn=1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// ************.  Operations ******
+let value=3
+let negValue=-value
+//console.log(negValue);
+let s1= "hello"
+let s2= "Reshav"
+let s3= s1+s2
+console.log(s3);
+
+//console.log(+true);will do conversion
+//console.log(true+);will give error
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
