645ce87df0328d63018491b2ecfea5993d503b33
Reshav Ravi <140899268+lawnge10@users.noreply.github.com>
2026-09-13 16:41:39 +0530

    till datatypessummary

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,4 +1,16 @@
 console.log("2" > 1)
 console.log("02" > 1)
 
-console.log();
+// console.log(null > 0);false
+// console.log(null == 0);false
+// console.log(null >=0);true
+
+//The reason is that an equality check == and comparison > < >= <= work differently .
+//Comparisons convert null to a number , treating it as 0.
+//That's why (3)null >=0 is true and (1)null>0 is false.
+
+//same problem will come with undefined it will always give false
+//In javascript comparison and equality check works diffrently
+
+//=== also checks value as well as datatype
+
---
+++ datatypesSummary.js
@@ -0,0 +1,37 @@
+//Primitive data types
+//7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+//const bigNumber= 2323414144515514
+
+
+
+
+//Refrence type or non-primitive type
+
+//Array,Objects,Functions
+//Arrays
+const heros=["shaktiman","naagraj","doga"]
+//Objects->they are written in curly braces
+let myObj={
+    name:"Reshav",
+    age:23,
+}
+//now lets understand Functions
+const myFunction=function(){
+     console.log("Hello world");
+     
+}
+
+
